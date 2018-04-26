70d5f7eac738f0f5ebfe4921d1931f46f1b50f36
lopexpex <38585485+lopexpex@users.noreply.github.com>
2018-04-26 16:41:03 +0100

    Revert "Revert "pp""
    This reverts commit e8a5d102340f68b6d2674830238f3bd6a0608fa0.

---
+++ Prototype_new/Assets/Scripts/Editor/AdjustCam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ScreenScript))]
+[ExecuteInEditMode]
+
+public class AdjustCam : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        ScreenScript screen = target as ScreenScript;
+        if (GUILayout.Button("TakeMeasures", GUILayout.Width(150)))
+        {
+           
+            screen.TakeMesures();
+        }
+        if (GUILayout.Button("CallCamera", GUILayout.Width(150)))
+        {
+
+            screen.CallCamera();
+        }
+
+    }
+}
