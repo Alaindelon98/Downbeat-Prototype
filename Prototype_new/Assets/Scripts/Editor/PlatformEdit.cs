53a63680c7aa6832b6adb6417dd13fe6959eec2e
adrinuga <38584756+adrinuga@users.noreply.github.com>
2018-05-15 21:31:49 +0200

    level2Lookat

---
+++ Prototype_new/Assets/Scripts/Editor/PlatformEdit.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(platformScript))]
+[ExecuteInEditMode]
+
+public class PlatformEdit : Editor
+{
+    platformScript platform;
+
+    void OnEnable()
+    {
+        platform = (platformScript)target;
+    }
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        platformScript platform = target as platformScript;
+
+        if (GUILayout.Button("AddPosition", GUILayout.Width(150)))
+        {
+            this.serializedObject.Update();
+
+            platform.positionsList.Add(platform.transform.position);
+
+            EditorUtility.SetDirty(target);
+
+            this.serializedObject.ApplyModifiedProperties();
+        }
+        if (GUILayout.Button("RemoveLast", GUILayout.Width(150)))
+        {
+
+            platform.positionsList.RemoveAt(platform.positionsList.Count-1);
+
+            EditorUtility.SetDirty(target);
+
+            
+        }
+        if (GUILayout.Button("ClearList", GUILayout.Width(150)))
+        {
+
+            platform.positionsList.Clear();
+
+            EditorUtility.SetDirty(target);
+
+
+        }
+    }
+
+}
