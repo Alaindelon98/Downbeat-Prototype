7f3b9ecae14df88fa6741916448b80367b35e4cd
annamde <annadiaz553@gmail.com>
2018-06-03 20:17:02 +0100

    poom del brillito

---
+++ Down Beat/Assets/Scripts/BrilloScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BrilloScript : BeatActor {
+
+	public Tilemap tilemap;
+
+	private Color original;
+
+	// Use this for initialization
+	void Start () {
+		SetBehavior();
+		original = tilemap.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if (BeatListener ()) {
+			tilemap.color = original;
+		}
+		else
+		{
+			tilemap.color = new Color (original.r, original.g, original.b, 0.75f);
+		}*/
+		if (BeatManager.fourthNotesCounter == 1) {
+			tilemap.color = original;
+		} else {
+			tilemap.color = new Color (original.r, original.g, original.b, 0.75f);
+		}
+	}
+}
