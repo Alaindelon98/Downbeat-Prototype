bd67328df2a812cc289334ae29fde08085e7fe5c
annamde <annadiaz553@gmail.com>
2018-06-15 04:05:42 +0100

    transiciones

---
+++ Down Beat/Assets/TransicionScipt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransicionScipt : MonoBehaviour {
+
+	public AudioSource song;
+
+	private int index;
+	// Use this for initialization
+	void Start () {
+		index = SceneManager.GetActiveScene ().buildIndex;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (song.time >=6.0f) {
+			SceneManager.LoadScene (index+1);
+			//print("changeee");
+		}
+	}
+}
